7b90893b9f3a7e7e63943a3fa455066d36495725
papadpickle <99397192+papadpickle@users.noreply.github.com>
2022-02-25 15:06:52 +0100

    atomic snapshot for orderbook struct to avoid potential data race

--- include/orderbook.hpp
+++ include/orderbook.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <spdlog/spdlog.h>
 
+#include <atomic>
 #include <functional>
 #include <mutex>
 
@@ -26,29 +27,25 @@ class orderbook {
 
   void updateCallback() {
     const std::lock_guard<std::mutex> lock(callbackMtx);
-    latestBook.highestBid = bids.getBestPrice();
-    latestBook.lowestAsk = asks.getBestPrice();
-    if (latestBook.valid()) {
-      latestBook.midPoint = (latestBook.lowestAsk + latestBook.highestBid) / 2;
-      latestBook.spreadBsp =
-          (((double)latestBook.lowestAsk - latestBook.highestBid) * 10000) /
-          latestBook.midPoint;
+    snapshot rx;
+    rx.highestBid = bids.getBestPrice();
+    rx.lowestAsk = asks.getBestPrice();
+    if (rx.valid()) {
+      rx.midPoint = (rx.lowestAsk + rx.highestBid) / 2;
+      rx.spreadBsp =
+          (((double)rx.lowestAsk - rx.highestBid) * 10000) / rx.midPoint;
+      latestBook.store(rx);
+      onUpdateCb();
     }
-    onUpdateCb();
   }
 
-  bool valid() const { return latestBook.valid(); }
-  uint64_t getHighestBid() const { return latestBook.highestBid; }
-  uint64_t getLowestAsk() const { return latestBook.lowestAsk; }
-  uint64_t getMidPoint() const { return latestBook.midPoint; }
-  double getSpreadBsp() const { return latestBook.spreadBsp; }
+  bool valid() const { return latestBook.load().valid(); }
+  uint64_t getHighestBid() const { return latestBook.load().highestBid; }
+  uint64_t getLowestAsk() const { return latestBook.load().lowestAsk; }
+  uint64_t getMidPoint() const { return latestBook.load().midPoint; }
+  double getSpreadBsp() const { return latestBook.load().spreadBsp; }
 
  private:
-  std::function<void()> onUpdateCb;
-  std::mutex callbackMtx;
-  subscription::bookSide& bids;
-  subscription::bookSide& asks;
-
   struct snapshot {
     uint64_t highestBid;
     uint64_t lowestAsk;
@@ -61,6 +58,11 @@ class orderbook {
     }
   };
 
-  snapshot latestBook;
+  std::atomic<snapshot> latestBook;
+  std::function<void()> onUpdateCb;
+  std::mutex callbackMtx;
+  subscription::bookSide& bids;
+  subscription::bookSide& asks;
+
 };
 }  // namespace mango_v3
