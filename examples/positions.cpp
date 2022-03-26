11c105e49f708e73ea43f66d9a70d66b86c61e93
clang-format <style@github.actions>
2022-03-26 22:26:28 +0000

    style fixup

--- examples/positions.cpp
+++ examples/positions.cpp
@@ -7,8 +7,9 @@ using json = nlohmann::json;
 int main() {
   const auto& config = mango_v3::DEVNET;
   auto connection = solana::rpc::Connection(config.endpoint);
-  const auto& mangoAccountInfo = connection.getAccountInfo<mango_v3::MangoAccountInfo>(
-      "9aWg1jhgRzGRmYWLbTrorCFE7BQbaz2dE5nYKmqeLGCW");
+  const auto& mangoAccountInfo =
+      connection.getAccountInfo<mango_v3::MangoAccountInfo>(
+          "9aWg1jhgRzGRmYWLbTrorCFE7BQbaz2dE5nYKmqeLGCW");
   const auto& mangoAccount = mango_v3::MangoAccount(mangoAccountInfo);
   spdlog::info(mangoAccountInfo.owner.toBase58());
   spdlog::info(mangoAccount.accountInfo.owner.toBase58());
